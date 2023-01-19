dd5fa2f1f58def28e306a80975abc6b912fde597
BryantVaughn <bryant.f.vaughn@gmail.com>
2023-01-18 17:38:17 -0700

    Add shop routes file

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ urlencoded: false }));
 
 app.use("/admin", adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
