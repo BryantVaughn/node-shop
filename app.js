5e79cb56b1ee3949ee86c7ef57362ff386e4399e
BryantVaughn <bryant.f.vaughn@gmail.com>
2023-01-18 17:43:13 -0700

    Import shop routes and catch bad paths

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ const shopRoutes = require("./routes/shop");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ urlencoded: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page Not Found...</h1>");
+});
 
 app.listen(3000);
