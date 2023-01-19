73d597ee8647dde040c74aed29a1e2beb6577f3e
BryantVaughn <bryant.f.vaughn@gmail.com>
2023-01-18 17:30:06 -0700

    Add simple admin routes

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
+const adminRoutes = require("./routes/admin");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ urlencoded: false }));
+
+app.use("/admin", adminRoutes);
 
 app.listen(3000);
